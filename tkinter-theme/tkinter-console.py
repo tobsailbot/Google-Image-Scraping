f30fc58a1d7a9929c6e2588ad43d9381544c8fe4
Tobias Pilarche <tobsailbot@gmail.com>
2023-06-04 19:07:17 -0300

    gui ready

--- tkinter-theme/tkinter-console.py
+++ tkinter-theme/tkinter-console.py
@@ -38,54 +38,59 @@ def run_thread():
 # Crear la ventana principal
 root = tk.Tk()
 # Main window configuration
-root.title("Premiere Zoom Tools")
+root.title("Google Images Downloader")
 root.resizable(False, False)
 
 main_frame = ttk.Frame(root)
-main_frame.pack(pady=(20,15))
+main_frame.pack(pady=(15,15))
 
-# crear label frame
-label_frame = ttk.LabelFrame(main_frame)
-label_frame.pack()
 
 # ingresar la cadena de busqueda
-label_1 = ttk.Label(label_frame, text="Search input", width=15)
-label_1.grid(column=0, row=0, pady=5)
-entry_1 = ttk.Entry(label_frame, width=30)
-entry_1.grid(column=1, row=0, pady=5, sticky="w")
+label_1 = ttk.Label(main_frame, text="Search input", width=12)
+label_1.grid(column=0, row=0, pady=(10,10), padx=(10,0))
+
+entry_1 = ttk.Entry(main_frame, width=24, font=("TkDefaultFont", 10))
+entry_1.grid(column=1, row=0,columnspan=3, pady=(10,10), padx=(0,10), sticky="w")
 
 # cantidad de imagenes
-label_2 = ttk.Label(label_frame, text="Images ammount", width=15)
-label_2.grid(column=0, row=1, pady=20)
-entry_1 = ttk.Entry(label_frame, width=30)
-entry_1.grid(column=1, row=1, pady=5, sticky="w")
+label_2 = ttk.Label(main_frame, text="Images count", width=12)
+label_2.grid(column=0, row=1, pady=(15,10), padx=(10,0))
+
+entry_1 = ttk.Entry(main_frame, width=24, font=("TkDefaultFont", 10))
+entry_1.grid(column=1, row=1,columnspan=3, pady=(15,10), sticky="w")
 
 # Crear un widget Checkbutton para activar/desactivar el modo transparente
-label_3 = ttk.Label(label_frame, text="Transparent", width=15)
-label_3.grid(column=0, row=2, pady=20)
-CheckVar = tk.IntVar(0)
-check_button = ttk.Checkbutton(label_frame, variable=CheckVar)
-check_button.grid(column=1, row=2, pady=5, sticky="w")
+label_3 = ttk.Label(main_frame, text="Transparent", width=12)
+label_3.grid(column=0, row=2, pady=(11,10), padx=(10,0))
 
-label_4 = ttk.Label(label_frame, text="Output folder", width=15)
-label_4.grid(column=0, row=5, pady=20)
-entry_4 = ttk.Entry(label_frame, width=30)
-entry_4.grid(column=1, row=5, pady=5, sticky="w")
+CheckVar = tk.IntVar(0)
+check_button = ttk.Checkbutton(main_frame, variable=CheckVar)
+check_button.grid(column=1, row=2, pady=(11,10), sticky="w")
+
+label_4 = ttk.Label(main_frame, text="Output folder", width=12)
+label_4.grid(column=0, row=3, pady=(11,15), padx=(10,0))
 
 # Crear un Button para seleccionar una carpeta
-browse_folder = ttk.Button(label_frame, text="Browse")
-browse_folder.grid(column=0, row=6, pady=10)
+browse_folder = ttk.Button(main_frame, text="Browse")
+browse_folder.grid(column=1, row=3, pady=(11,15), sticky="w")
 
+label_4 = ttk.Label(main_frame, text="./folder_output", width=12)
+label_4.grid(column=2, row=3, pady=(11,15), padx=(0,0), sticky="w")
 
+separator = ttk.Separator(main_frame, orient="horizontal")
+separator.grid(column=0, row=4, columnspan=3, sticky="ew", pady=10, padx=(10,0))
 
-
-# button = ttk.Button(label_frame, text="Download Images", command=run_thread, style="Accent.TButton", padding=(10, 5))
-# button.grid(column=0, row=1, pady=20)
+button = ttk.Button(main_frame, text="Download Images", command=run_thread, style="Accent.TButton", padding=(10, 5))
+button.grid(column=0, row=5, columnspan=3, pady=10)
 
 
 # Crear un widget Text para mostrar la salida de la consola
-text_widget = tk.Text(root, width=40, height=8, font=("Consolas", 12), wrap=tk.WORD, blockcursor=True, state=tk.DISABLED)
-text_widget.pack()
+text_widget = tk.Text(main_frame, width=40, height=7, font=("Consolas", 10), wrap=tk.WORD, blockcursor=True, state=tk.DISABLED)
+text_widget.configure(state=tk.NORMAL)
+text_widget.insert(tk.END, 'Download status... \n')
+text_widget.configure(state=tk.DISABLED)
+text_widget.see(tk.END)
+text_widget.grid(column=0, row=6, columnspan=3, sticky="ew", pady=10, padx=(10,0))
 
 
 
